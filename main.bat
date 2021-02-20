20bbbe31bd272cff522f20409511e17cf3b0094e
MikhailSorokin <sorokinmike96@gmail.com>
2021-02-20 15:32:31 -0500

    Found problem with checkbox page being cached - made sure to ONLY check if that checkbox was not already checked off as it will try to check it off again and also checking off the right element div.

--- main.bat
+++ main.bat
@@ -6,20 +6,20 @@ rem - conda to be in the PATH
 rem - cmd.exe to be initialized with conda init
 
 rem Define here the path to your conda installation
-set CONDAPATH=C:\ProgramData\Anaconda3
+set CONDAPATH=C:\Users\Demo2\anaconda3
 rem Define here the name of the environment
-set ENVNAME=eppley-bot
+set ENVNAME=symptons-bot
 
 rem The following command activates the base environment.
 rem call C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
-if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=C:\Users\rober\.conda\envs\eppley-bot)
+if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=C:\Users\Demo2\anaconda3\envs\symptons-bot)
 
 rem Activate the conda environment
 rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
 call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
 
 rem Run a python script in that environment
-python3 "C:\Users\rober\Google Drive\PersonalProjects\Symptoms-Monitoring-Bot\main.py"
+python "D:\Github\Symptoms-Monitoring-Bot\main.py"
 
 rem Deactivate the environment
 call conda deactivate
@@ -31,3 +31,5 @@ rem conda deactivate
 
 rem One could also use the conda run command
 rem conda run -n someenv python script.py
+
+pause
